ec243dafcc9a10aa0deba0d69bf800d252dc56c5
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2026-03-02 17:21:26 +0530

    chore: Convert the python services into workspaces

--- server/core/settings.py
+++ server/core/settings.py
@@ -1,14 +1,18 @@
+from pathlib import Path
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ROOT_DIR = Path(__file__).parent.parent.parent
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        case_sensitive=True, env_file="./../.env", extra="ignore"
+        case_sensitive=True, env_file=ROOT_DIR / ".env", extra="ignore"
     )
     api_key: str = Field(alias="LIVEKIT_API_KEY")
     api_secret: str = Field(alias="LIVEKIT_API_SECRET")
     server_url: str = Field(alias="LIVEKIT_URL")
 
 
-settings = Settings(**{})
+settings = Settings()
