f24bb8f6e533689317c172ce9bd27d9311e9998d
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2026-03-02 23:51:54 +0530

    feat: Add database engine

---
+++ shared/core/settings.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ROOT_DIR = Path(__file__).parent.parent.parent
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=True, env_file=ROOT_DIR / ".env", extra="ignore"
+    )
+    database_url: str = Field(alias="DATABASE_URL")
+
+
+settings = Settings()
---
+++ shared/db/engine.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import AsyncSession, sessionmaker
+
+from core.settings import settings
+
+async_engine = create_async_engine(settings.database_url, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
