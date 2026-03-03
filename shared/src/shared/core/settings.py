2185ebae3b35784ea05f627f65b52fdaff77e798
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2026-03-03 17:59:33 +0530

    feat: Add alembic and generate migration script

--- shared/src/shared/core/settings.py
+++ shared/src/shared/core/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-ROOT_DIR = Path(__file__).parent.parent.parent
+ROOT_DIR = Path(__file__).parent.parent.parent.parent.parent
 
 
 class Settings(BaseSettings):
